14da149dfcbb0826d57b41fe41a6d0d82c1558c4
inafi <irfan.k.nafi@gmail.com>
2020-07-27 13:04:46 -0400

    smooth scroll

--- JS/home.js
+++ JS/home.js
@@ -1,4 +1,28 @@
 function initialize() {
+    const slider = document.querySelector('.b3 ');
+    let isDown = false;
+    let startX;
+    let scrollLeft;
+
+    slider.addEventListener('mousedown', (e) => {
+        isDown = true;
+        startX = e.pageX - slider.offsetLeft;
+        scrollLeft = slider.scrollLeft;
+    });
+    slider.addEventListener('mouseleave', () => {
+        isDown = false;
+    });
+    slider.addEventListener('mouseup', () => {
+        isDown = false;
+    });
+    slider.addEventListener('mousemove', (e) => {
+        if (!isDown) return;
+        e.preventDefault();
+        const x = e.pageX - slider.offsetLeft;
+        const walk = (x - startX) * 3; //scroll-fast
+        slider.scrollLeft = scrollLeft - walk;
+    });
+
     setInterval(function () {
         if ($(window).scrollTop() == 0) {
             $("nav").css('background-color', 'transparent');
@@ -19,6 +43,14 @@ function initialize() {
             $("nav *").css('color', '#1B98E0');
             $("nav span:hover").css('color', '#06D6A0');
         }
+        var timewidth = $('.b3 .timeline > :last-child').offset().left - $('.b3 .timeline > :first-child')
+            .offset()
+            .left + $('.b3 .timeline > :last-child').width();
+        var linewidth = timewidth - $('.b3 .timeline > :first-child').width() / 2 - $(
+            '.b3 .timeline > :last-child').width() / 2;
+        $(".b3 .timeline-wrap").width(timewidth + "px");
+        $(".b3 .timeline").width(timewidth + "px");
+        $(".b3 .line").width(linewidth + "px");
     }, 20)
 }
 $(initialize)

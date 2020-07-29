e48b92c430b43ae25d84db433877d8c5b2606c8a
inafi <irfan.k.nafi@gmail.com>
2020-07-28 23:18:21 -0400

    demo

--- JS/home.js
+++ JS/home.js
@@ -50,11 +50,14 @@ function initialize() {
             if ($(window).scrollTop() == 0 && !isMobile && $(window).width() > 1170) {
                 $("nav").css('background-color', 'transparent');
                 $("nav").css('box-shadow', 'none');
+                $("nav").css('height', 'calc(5vh + 2vw)');
+                $("nav .left").css('padding-top', '2vh');
                 $("nav .right").css('padding-top', '2vh');
                 $("nav *").css('color', '#fff');
                 $("nav span:hover").css('color', '#000');
             } else {
                 $("nav").css('background-color', '#fff');
+                $("nav").css('height', 'calc(3vh + 2vw)');
                 $("nav").css('box-shadow', `
                 0 2.8px 2.2px rgba(0, 0, 0, 0.014),
                 0 6.7px 5.3px rgba(0, 0, 0, 0.018),
@@ -62,6 +65,7 @@ function initialize() {
                 0 2.3px 2px rgba(0, 0, 0, 0.022),
                 0 3.8px 2px rgba(0, 0, 0, 0.026),
                 0 4px 5px rgba(0, 0, 0, 0.05)`);
+                $("nav .left").css('padding-top', '0vh');
                 $("nav .right").css('padding-top', '0vh');
                 $("nav *").css('color', '#1B98E0');
                 $("nav span:hover").css('color', '#06D6A0');
